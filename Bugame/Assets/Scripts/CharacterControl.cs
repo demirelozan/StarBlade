c5aa276d1c236f926bd08352a503310bc0f09b42
demirelozan <57192515+demirelozan@users.noreply.github.com>
2020-02-29 01:40:32 +0300

    Movement added

---
+++ Bugame/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CharacterControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    LayerMask groundLayer;
+
+    private CircleCollider2D circleCollider;
+
+    [SerializeField]
+    private bool isGrounded()
+    {   
+        RaycastHit2D raycastHit = Physics2D.Raycast(circleCollider.bounds.center, Vector2.down,circleCollider.bounds.extents.y + 0.1f,groundLayer);
+        Color rayColor;
+        if(raycastHit.collider != null)
+        {
+            rayColor = Color.green;    
+
+        }
+        else
+        {
+            rayColor = Color.red;
+
+        }
+        Debug.DrawRay(circleCollider.bounds.center, Vector2.down, rayColor);
+
+        return raycastHit.collider!= null;
+    }
+
+
+    private float horizontal;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        circleCollider = GetComponent<CircleCollider2D>();
+    }
+
+    
+    
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded())
+        {
+            rb.AddForce(new Vector2(0, 150));
+        }
+
+    }
+}
